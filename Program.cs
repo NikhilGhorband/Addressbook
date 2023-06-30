bbfdbc636babc23122a3e790af14b16a1e53af38
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-07-01 04:43:27 +0530

    [Nikhil]Add: Add Contat

--- Program.cs
+++ Program.cs
@@ -1,14 +1,41 @@
-﻿namespace AddressbookProblems
+﻿using AddressbookProblems;
+
+namespace AddressbookProblem
 {
+    {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("...Welcome to Address Book...");
             AddressBook addressBook = new AddressBook();
-            addressBook.CreateContact();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n4.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBook.CreateContact();
+                        break;
+                    case 2:
+                        addressBook.AddContact();
+                        break;
+                    case 3:
+                        addressBook.DisplayContact();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct number ! ");
+                        break;
+                }
+            }
             Console.ReadLine();
-
         }
     }
+
+}
 }
